e9389b2c02a5c668dce42ab324ea8280a06a81cd
alex.ochs2108 <alex.ochs2108@gmail.com>
2018-04-02 02:25:48 +0200

    basis

--- InstaLocker/Program.cs
+++ InstaLocker/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
 namespace InstaLocker
 {
@@ -10,6 +12,22 @@ namespace InstaLocker
     {
         static void Main(string[] args)
         {
+            //input champion name
+            Console.Write("Champion (Press Enter): ");
+            String champion = Console.ReadLine();
+
+            //move cursor from corner to champion filter and click
+
+            //type in champion name
+
+            //move cursor to champion and click
+
+            //move cursor to lock-in and click
+        }
+
+        private void MoveCursor(int x, int y)
+        {
+            Cursor.Position = new Point(x, y);
         }
     }
 }
